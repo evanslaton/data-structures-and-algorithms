153f331d76d829f3c0dad7aca54f19508f9cbb80
Evan Slaton <evanslaton@gmail.com>
2019-05-09 23:52:36 -0700

    Finished containsDuplicates

--- src/main/java/labs/Lab02.java
+++ src/main/java/labs/Lab02.java
@@ -1,12 +1,20 @@
 package labs;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
 
 public class Lab02 {
     public static void main(String[] args) {
         int[] rolls = roll(4);
         System.out.println(Arrays.toString(rolls));
+
+        int[] intArray = {1, 2, 3, 4, 5};
+        System.out.println(containsDuplicates(intArray));
+
+        int[] intArray2 = {1, 2, 3, 4, 4};
+        System.out.println(containsDuplicates(intArray2));
     }
 
     // Write a method called roll that accepts an integer n and
@@ -27,5 +35,23 @@ public class Lab02 {
         int MAXIMUM_NUMBER = 6;
         Random random = new Random();
         return random.nextInt( MAXIMUM_NUMBER - MINIMUM_NUMBER + 1) + MINIMUM_NUMBER;
+    }
+
+    // Write a method called containsDuplicates that returns true
+    // or false depending on whether the array contains duplicate
+    // values.
+    public static boolean containsDuplicates(int[] arr) {
+        Set<Integer> set = arrayToSet(arr);
+        return set.size() == arr.length ? false : true;
+    }
+
+    public static Set<Integer> arrayToSet(int[] arr) {
+        Set<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            set.add(arr[i]);
+        }
+
+        return set;
     }
 }
