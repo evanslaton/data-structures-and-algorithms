6c67db4bd30fe55e059f2a6df4a69272f2d40ee1
Evan Slaton <evanslaton@gmail.com>
2019-05-11 00:34:37 -0700

    Minor adjustment

--- src/main/java/labs/Lab03.java
+++ src/main/java/labs/Lab03.java
@@ -21,8 +21,7 @@ public class Lab03 {
         votes.add("Hedge");
         votes.add("Shrub");
         votes.add("Bush");
-        votes.add("Hedge");
-        votes.add("Bush");
+        votes.add("Hedge");;
 
         String winner = tally(votes);
         System.out.println(winner + " received the most votes!");
