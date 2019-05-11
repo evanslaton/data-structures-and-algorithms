59f5399212914ec0605d3de43a8f89272da9270f
Evan Slaton <evanslaton@gmail.com>
2019-05-10 18:09:56 -0700

    Finished getLowestAverage

--- src/main/java/labs/Lab02.java
+++ src/main/java/labs/Lab02.java
@@ -15,6 +15,8 @@ public class Lab02 {
 
         int[] intArray2 = {1, 2, 3, 4, 4};
         System.out.println(containsDuplicates(intArray2));
+
+        System.out.println(calculateAverage(intArray));
     }
 
     // Write a method called roll that accepts an integer n and
@@ -53,5 +55,39 @@ public class Lab02 {
         }
 
         return set;
+    }
+
+    // Write a method that accepts an array of integers and calculates
+    // and returns the average of all the values in the array.
+    public static double calculateAverage(int[] arr) {
+        return reduceArray(arr) / arr.length;
+    }
+
+    public static double reduceArray(int[] arr) {
+        double output = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            output+= arr[i];
+        }
+
+        return output;
+    }
+
+    // Given an array of arrays calculate the average value for each
+    // array and return the array with the lowest average.
+    public static int[] getLowestAverage(int[][] arrayOfArrays) {
+        int indexOfLowestAverage = 0;
+        double lowestAverage = calculateAverage(arrayOfArrays[0]);
+        double currentAverage;
+
+        for (int i = 1; i < arrayOfArrays.length; i++) {
+            currentAverage = calculateAverage(arrayOfArrays[i]);
+            if (currentAverage < lowestAverage) {
+                indexOfLowestAverage = i;
+                lowestAverage = currentAverage;
+            }
+        }
+
+        return arrayOfArrays[indexOfLowestAverage];
     }
 }
